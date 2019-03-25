668a454e09d141ff1871acc2a2b13e014d2aa976
Mark Michelson <mmichels@redhat.com>
2019-03-25 15:41:03 -0400

    Add variables for OVS source and lib dirs.

--- northd/automake.mk
+++ northd/automake.mk
@@ -4,7 +4,7 @@ northd_ovn_northd_SOURCES = northd/ovn-northd.c
 northd_ovn_northd_LDADD = \
 	lib/libovn.la \
 	ovs/ovsdb/libovsdb.la \
-	ovs/lib/libopenvswitch.la
+	$(OVS_LIBDIR)/libopenvswitch.la
 man_MANS += northd/ovn-northd.8
 EXTRA_DIST += northd/ovn-northd.8.xml
 CLEANFILES += northd/ovn-northd.8
