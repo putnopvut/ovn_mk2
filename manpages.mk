43bee700b44e068d152ef391ff75d0ed1fb8ebd9
Mark Michelson <mmichels@redhat.com>
2019-03-25 17:45:59 -0400

    Use a variable for manpages directory.

--- manpages.mk
+++ manpages.mk
@@ -1,127 +1,29 @@
 # Generated automatically -- do not modify!    -*- buffer-read-only: t -*-
 
-ovs/ovsdb/ovsdb-client.1: \
-	ovs/ovsdb/ovsdb-client.1.in \
-	lib/common-syn.man \
-	lib/common.man \
-	lib/daemon-syn.man \
-	lib/daemon.man \
-	lib/ovs.tmac \
-	lib/ssl-bootstrap-syn.man \
-	lib/ssl-bootstrap.man \
-	lib/ssl-connect-syn.man \
-	lib/ssl-connect.man \
-	lib/ssl-syn.man \
-	lib/ssl.man \
-	lib/table.man \
-	lib/vlog-syn.man \
-	lib/vlog.man \
-	ovsdb/ovsdb-schemas.man
-ovs/ovsdb/ovsdb-client.1.in:
-lib/common-syn.man:
-lib/common.man:
-lib/daemon-syn.man:
-lib/daemon.man:
-lib/ovs.tmac:
-lib/ssl-bootstrap-syn.man:
-lib/ssl-bootstrap.man:
-lib/ssl-connect-syn.man:
-lib/ssl-connect.man:
-lib/ssl-syn.man:
-lib/ssl.man:
-lib/table.man:
-lib/vlog-syn.man:
-lib/vlog.man:
-ovsdb/ovsdb-schemas.man:
-
-ovs/ovsdb/ovsdb-server.1: \
-	ovs/ovsdb/ovsdb-server.1.in \
-	lib/common-syn.man \
-	lib/common.man \
-	lib/coverage-unixctl.man \
-	lib/daemon-syn.man \
-	lib/daemon.man \
-	lib/memory-unixctl.man \
-	lib/ovs.tmac \
-	lib/service-syn.man \
-	lib/service.man \
-	lib/ssl-bootstrap-syn.man \
-	lib/ssl-bootstrap.man \
-	lib/ssl-connect-syn.man \
-	lib/ssl-connect.man \
-	lib/ssl-peer-ca-cert-syn.man \
-	lib/ssl-peer-ca-cert.man \
-	lib/ssl-syn.man \
-	lib/ssl.man \
-	lib/unixctl-syn.man \
-	lib/unixctl.man \
-	lib/vlog-syn.man \
-	lib/vlog-unixctl.man \
-	lib/vlog.man
-ovs/ovsdb/ovsdb-server.1.in:
-lib/common-syn.man:
-lib/common.man:
-lib/coverage-unixctl.man:
-lib/daemon-syn.man:
-lib/daemon.man:
-lib/memory-unixctl.man:
-lib/ovs.tmac:
-lib/service-syn.man:
-lib/service.man:
-lib/ssl-bootstrap-syn.man:
-lib/ssl-bootstrap.man:
-lib/ssl-connect-syn.man:
-lib/ssl-connect.man:
-lib/ssl-peer-ca-cert-syn.man:
-lib/ssl-peer-ca-cert.man:
-lib/ssl-syn.man:
-lib/ssl.man:
-lib/unixctl-syn.man:
-lib/unixctl.man:
-lib/vlog-syn.man:
-lib/vlog-unixctl.man:
-lib/vlog.man:
-
-ovs/ovsdb/ovsdb-tool.1: \
-	ovs/ovsdb/ovsdb-tool.1.in \
-	lib/common-syn.man \
-	lib/common.man \
-	lib/ovs.tmac \
-	lib/vlog-syn.man \
-	lib/vlog.man \
-	ovsdb/ovsdb-schemas.man
-ovs/ovsdb/ovsdb-tool.1.in:
-lib/common-syn.man:
-lib/common.man:
-lib/ovs.tmac:
-lib/vlog-syn.man:
-lib/vlog.man:
-ovsdb/ovsdb-schemas.man:
-
 utilities/ovn-detrace.1: \
 	utilities/ovn-detrace.1.in \
-	ovs/lib/common-syn.man \
-	ovs/lib/common.man \
-	ovs/lib/ovs.tmac
+	lib/common-syn.man \
+	lib/common.man \
+	lib/ovs.tmac
 utilities/ovn-detrace.1.in:
-ovs/lib/common-syn.man:
-ovs/lib/common.man:
-ovs/lib/ovs.tmac:
+lib/common-syn.man:
+lib/common.man:
+lib/ovs.tmac:
 
 utilities/ovn-sbctl.8: \
 	utilities/ovn-sbctl.8.in \
+	lib/common.man \
+	lib/db-ctl-base.man \
+	lib/ovs.tmac \
+	lib/ssl-bootstrap.man \
+	lib/ssl.man \
 	lib/table.man \
-	ovs/lib/common.man \
-	ovs/lib/db-ctl-base.man \
-	ovs/lib/ovs.tmac \
-	ovs/lib/ssl-bootstrap.man \
-	ovs/lib/ssl.man \
-	ovs/lib/vlog.man
+	lib/vlog.man
 utilities/ovn-sbctl.8.in:
+lib/common.man:
+lib/db-ctl-base.man:
+lib/ovs.tmac:
+lib/ssl-bootstrap.man:
+lib/ssl.man:
 lib/table.man:
-ovs/lib/common.man:
-ovs/lib/db-ctl-base.man:
-ovs/lib/ovs.tmac:
-ovs/lib/ssl-bootstrap.man:
-ovs/lib/ssl.man:
-ovs/lib/vlog.man:
+lib/vlog.man:
